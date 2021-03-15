d048fed166331f2c9facb38a967da3d353c2f6af
andersonarorjdev <anderson.r.o.r.jr@gmail.com>
2021-03-15 09:34:43 -0300

    Added annotations about constant and References in php

---
+++ Value|Reference.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 'Anderson';
+    $b = null;
+
+    $b = $a; //This is an normal atribuition.
+
+    //Reference atribuition
+    $b = &$a; //$a and $b will change togheter.
+    
+?>
---
+++ constant.php
@@ -0,0 +1,11 @@
+<?php
+    define('NAME', 'Anderson');
+
+    //Can be defined using word define, this is similar to Javascript.
+
+    const USERNAME = 'anderson';
+
+    echo NAME;
+
+     // NAME = 'Daniel'; // will print error because NAME cant change.;
+?>
